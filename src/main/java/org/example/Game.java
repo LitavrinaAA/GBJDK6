9ada41a2a0b2faa0521ad2944067df33d61dd252
LitavrinaA <a.a.litavrina@gmail.com>
2024-03-06 21:48:06 +0300

    еще изменения

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -11,19 +11,38 @@ import java.util.Random;
 public class Game {
     @Getter
     private int prizeDoor;
-    @Getter private int openDoor;
-    @Getter private int otherDoor;
+    @Getter
+    private int openDoor;
+    @Getter
+    private int otherDoor;
+    List<Integer> doors = new ArrayList<>(List.of(1, 2, 3));
     Random generator = new Random();
 
-    public void refreshDoors() {
-        List<Integer> doors = new ArrayList<>(List.of(1, 2, 3));
-        Collections.shuffle(doors, generator);
+    public void openNoPrizeDoor(int userDoor) {
 
-        prizeDoor = doors.get(0);
-        openDoor = doors.get(1);
-        otherDoor = doors.get(2);
+        if (userDoor == prizeDoor) {
+            if (generator.nextBoolean()) {
+                otherDoor = doors.get(1);
+                openDoor = doors.get(2);
+            }
+        } else {
+            if (userDoor == doors.get(1)) {
+                openDoor = doors.get(2);
+                otherDoor = doors.get(1);
+            } else {
+                openDoor = doors.get(1);
+                otherDoor = doors.get(2);
+            }
+        }
     }
-    public boolean resultOfGame(int userDoor){
+
+
+    public void putPrize() {
+        Collections.shuffle(doors, generator);
+        prizeDoor = doors.get(0);
+    }
+
+    public boolean resultOfGame(int userDoor) {
         //Проверим, угадал ли пользователь
         return userDoor == prizeDoor;
     }
